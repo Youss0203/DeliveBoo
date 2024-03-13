52967657fb439c19aae7faeab46d4ee5e01553de
BernardoBarbucci <bernardobarbucci@gmail.com>
2024-03-13 11:39:14 +0100

    problema edit e update relativo a prezzo ed img risolto

--- app/Http/Controllers/Admin/DishesController.php
+++ app/Http/Controllers/Admin/DishesController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Validation\Rules\File;
 use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Validator;
 
 class DishesController extends Controller
 {
@@ -45,15 +46,22 @@ class DishesController extends Controller
      */
     public function store(Request $request)
     {
-        $data = $request->validate($this->rules);
+        $request->validate([
+            'name' => ['required', 'string', 'min:3', 'max:40'],
+            'img_url' => ['required', 'image'],
+            'price' => ['required', 'numeric', 'min:0'], // Modifica la validazione del prezzo
+            'ingredients' => ['required', 'string'],
+            'description' => ['required', 'string'],
+        ]);
 
-        $data = $request->validate(['price' => 'required|numeric|min:0',]); // validazione prezzo per evitare che sia negativo
-
+        $data = $request->all();
         $data['visibility'] = isset($data['visibility']);
         $data['restaurant_id'] = Restaurant::where('id', Auth::id())->pluck('id')->first();
 
-        $imageSrc = Storage::put('uploads/dishes', $data['img_url']);
-        $data['img_url'] = $imageSrc;
+        if ($request->hasFile('img_url')) {
+            $imageSrc = Storage::put('uploads/dishes', $data['img_url']);
+            $data['img_url'] = $imageSrc;
+        }
 
         $newDish = Dish::create($data);
         return redirect()->route('admin.dishes.show', $newDish);
@@ -90,11 +98,25 @@ class DishesController extends Controller
      */
     public function update(Request $request, Dish $dish)
     {
-        $data = $request->validate($this->rules);
+        $validator = Validator::make($request->all(), $this->rules, [
+            'price.min' => 'Il prezzo deve essere almeno 0',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        $data = $request->all();
         $data['visibility'] = isset($data['visibility']);
 
-        $imageSrc = Storage::put('uploads/dishes', $data['img_url']);
-        $data['img_url'] = $imageSrc;
+        if (isset($data['price'])) {
+            $data['price'] = max(0, $data['price']);
+        }
+
+        if ($request->hasFile('img_url')) {
+            $imageSrc = Storage::put('uploads/dishes', $data['img_url']);
+            $data['img_url'] = $imageSrc;
+        }
 
         $dish->update($data);
 
