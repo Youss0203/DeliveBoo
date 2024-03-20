4e43c9612e45d0ed7e4e1646ff7d5f1bc78c749a
BernardoBarbucci <bernardobarbucci@gmail.com>
2024-03-20 13:06:11 +0100

    modifiche per orders

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,6 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+    // fillable per gli ordini 
+    protected $fillable = [
+        'customer_name', 'customer_lastname', 'customer_address', 'customer_phone',
+        // dovrebbe esserci anche total_amount ma va passato dal prezzo tot del carrello oppure lasciato stare 
+    ];
 
     public function dishes()
     {
